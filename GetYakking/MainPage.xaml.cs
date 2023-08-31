208b214fff8534d13a14cf0a6a186d07f839e849
Sacha Miller <SachaM@forrit.com>
2023-08-31 14:23:56 +0100

    update

--- GetYakking/MainPage.xaml.cs
+++ GetYakking/MainPage.xaml.cs
@@ -11,7 +11,7 @@
         private async void FadeInWelcomeMessage()
         {
             await WelcomeGrid.FadeTo(1, 2000); // Fade in over 2 seconds
-            await Task.Delay(20000); // Wait for 20 seconds
+            await Task.Delay(10000); // Wait for 20 seconds
             await WelcomeGrid.FadeTo(0, 2000); // Fade out over 2 seconds
             WelcomeGrid.IsVisible = false;
         }
