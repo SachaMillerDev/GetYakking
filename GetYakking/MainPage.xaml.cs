f5711a2cb516b6089f38f04a65cb0ac219161fac
Sacha Miller <SachaM@forrit.com>
2023-08-31 14:09:26 +0100

    update

--- GetYakking/MainPage.xaml.cs
+++ GetYakking/MainPage.xaml.cs
@@ -2,12 +2,18 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            FadeInWelcomeMessage();
         }
 
+        private async void FadeInWelcomeMessage()
+        {
+            await WelcomeGrid.FadeTo(1, 2000); // Fade in over 2 seconds
+            await Task.Delay(20000); // Wait for 20 seconds
+            await WelcomeGrid.FadeTo(0, 2000); // Fade out over 2 seconds
+            WelcomeGrid.IsVisible = false;
+        }
     }
 }
